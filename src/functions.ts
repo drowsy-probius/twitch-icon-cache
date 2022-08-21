2122279211c4555a94a3706a8bcc6b3b3fdc6ccd
k123s456h <k123s456h@gmail.com>
2022-08-22 04:06:53 +0900

    Update string.at to string.slice

--- src/functions.ts
+++ src/functions.ts
@@ -1,7 +1,7 @@
 import { STREAMER_DATA } from "./data";
 
 export const timeParser = (timeString: string, miliseconds=true) => {
-  const unit = typeof(timeString.slice(-1)) === "string" ? timeString.at(-1) : 's';
+  const unit = typeof(timeString.slice(-1)) === "string" ? timeString.slice(-1) : 's';
   const value = Number(timeString.slice(0, -1));
   const multiplier = miliseconds ? 1000 : 1;
   switch(unit)
