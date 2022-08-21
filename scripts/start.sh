95d871b070050c98b26f136a4cdb6f100f3b120e
k123s456h <k123s456h@gmail.com>
2022-08-22 03:43:31 +0900

    update script

--- scripts/start.sh
+++ scripts/start.sh
@@ -5,5 +5,8 @@ SRC_DIR="$SCRIPT_DIR/.."
 cd "$SRC_DIR"
 
 export NODE_ENV="development"
+export TWITCH_ICON_CACHE_PORT="32189"
+export TWITCH_ICON_CACHE_HOST="0.0.0.0"
+export TWITCH_ICON_CACHE_CACHE_TIME="2w"
 
 yarn dev
