2a468bb5f657cc61a723f6f927f7fd588251400f
k123s456h <k123s456h@gmail.com>
2022-08-30 21:47:47 +0900

    Add timestamp checker to /list/streamer API

--- src/router/handlers/list.ts
+++ src/router/handlers/list.ts
@@ -7,6 +7,8 @@ import { INDEX_FILE } from "../../constants";
 import { getRootFromRequest } from "../../functions";
 
 import checkStreamer from "./checkStreamer";
+import { IconIndex } from "../../@types/interfaces";
+import logger from "../../logger";
 
 const router = Router({mergeParams: true});
 const basePath = resolve(".");
@@ -17,6 +19,7 @@ const rootHandler = (req: Request, res: Response, next: NextFunction) => {
 
 const listHandler = (req: Request, res: Response, next: NextFunction) => {
   const streamer = req.params.streamer;
+  const timestamp = Number(req.query.ts || 0);  
   const requestedURL = getRootFromRequest(req);
   const jsonPath = resolve(`./images/${streamer}/${INDEX_FILE}`);
 
@@ -31,7 +34,15 @@ const listHandler = (req: Request, res: Response, next: NextFunction) => {
   const data = fs.readFileSync(jsonPath, "utf8");
   const regexp = new RegExp(basePath, "g");
   const uriReplacedData = data.replace(regexp, requestedURL);
-  const jsonData = JSON.parse(uriReplacedData);
+  const jsonData: IconIndex = JSON.parse(uriReplacedData);
+  if(timestamp === jsonData.timestamp)
+  {
+    return res.status(200).json({
+      status: false,
+      message: `your data is not outdated.`
+    })
+  }
+
   return res.status(200).json(jsonData);
 }
 
