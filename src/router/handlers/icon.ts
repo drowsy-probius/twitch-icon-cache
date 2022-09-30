16730c6065bebc38bd90992ce0b13aa46f7f3593
k123s456h <k123s456h@gmail.com>
2022-09-30 13:00:50 +0900

    Add icon api skeleton

--- src/router/handlers/icon.ts
+++ src/router/handlers/icon.ts
@@ -1,10 +1,30 @@
-import { Request, Response, Router } from "express";
+import { NextFunction, Request, Response, Router } from "express";
 import { resolve, join } from "path";
 
 const router = Router({ mergeParams: true });
 
-const handler = (req: Request, res: Response) => {
-  return res.status(200).json(1);
+const loginCheckHandler = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  next();
 };
+
+const uploadHandler = (req: Request, res: Response) => {
+  return res.status(200).json("upload");
+};
+
+const updateHandler = (req: Request, res: Response) => {
+  return res.status(200).json("update");
+};
+
+const deleteHandler = (req: Request, res: Response) => {
+  return res.status(200).json("delete");
+};
+
+router.post("/upload", loginCheckHandler, uploadHandler);
+router.post("/update", loginCheckHandler, updateHandler);
+router.post("/delete", loginCheckHandler, deleteHandler);
 
 export default router;
