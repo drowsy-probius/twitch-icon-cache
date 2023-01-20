0d2816bf68a48bfac0605ef776d4897f146fb735
drowsy-probius <k123s456h@gmail.com>
2023-01-21 04:57:37 +0900

    ffix: add error handlers

--- src/iconProcessor/bridgebbcc.ts
+++ src/iconProcessor/bridgebbcc.ts
@@ -3,34 +3,47 @@ import { createHash } from "crypto";
 import { extname } from "path";
 import { existsSync, mkdirSync } from "fs";
 
-import { 
-  IconIndexBridgeBBCC, 
-  Icon, 
+import {
+  IconIndexBridgeBBCC,
+  Icon,
   IconProps,
   IconBridgeBBCC,
-  StreamerData
+  StreamerData,
 } from "../@types/interfaces";
 import { FAILED_LIST_FILE, INDEX_FILE } from "../constants";
 import {
   getImageBasePath,
   getThumbnailBasePath,
-  saveImage, 
-  saveThumbnail, 
+  saveImage,
+  saveThumbnail,
   saveJsonFile,
 } from "../functions";
 
 import Logger from "../logger";
 
-
-export const indexDownloader = async (url: string): Promise<IconIndexBridgeBBCC> => {
-  const res = await axios.get(encodeURI(decodeURI(`${url}${url.includes("?") ? "&" : "?"}ts=${Date.now()}`)));
-  const jsonString = res.data.replace("dcConsData = ", `{"dcConsData" : `).replace(/;$/, "}");
-  const parsedJsonString = jsonString.replace(/\\"|"(?:\\"|[^"])*"|(\/\/.*|\/\*[\s\S]*?\*\/)/g, (m: any, g: any) => g ? "" : m);
+export const indexDownloader = async (
+  url: string
+): Promise<IconIndexBridgeBBCC> => {
+  const res = await axios.get(
+    encodeURI(
+      decodeURI(`${url}${url.includes("?") ? "&" : "?"}ts=${Date.now()}`)
+    )
+  );
+  const jsonString = res.data
+    .replace("dcConsData = ", `{"dcConsData" : `)
+    .replace(/;$/, "}");
+  const parsedJsonString = jsonString.replace(
+    /\\"|"(?:\\"|[^"])*"|(\/\/.*|\/\*[\s\S]*?\*\/)/g,
+    (m: any, g: any) => (g ? "" : m)
+  );
   const jsonData: IconIndexBridgeBBCC = JSON.parse(parsedJsonString);
   return jsonData;
-}
+};
 
-export const processor = async (streamer: StreamerData, jsonData: IconIndexBridgeBBCC): Promise<void> => {
+export const processor = async (
+  streamer: StreamerData,
+  jsonData: IconIndexBridgeBBCC
+): Promise<void> => {
   const logger = Logger(`${module.filename} [${streamer.name}]`);
   const basePath = getImageBasePath(streamer.name);
   const basePathThumbnail = getThumbnailBasePath(streamer.name);
@@ -39,161 +52,189 @@ export const processor = async (streamer: StreamerData, jsonData: IconIndexBridg
   let imageBaseUrl = "";
   let imagePropsName: IconProps;
 
-  if(!existsSync(basePath)) mkdirSync(basePath, {recursive: true});
-  if(!existsSync(basePathThumbnail)) mkdirSync(basePathThumbnail, {recursive: true});
+  if (!existsSync(basePath)) mkdirSync(basePath, { recursive: true });
+  if (!existsSync(basePathThumbnail))
+    mkdirSync(basePathThumbnail, { recursive: true });
 
   const findIconUri = async (icon: IconBridgeBBCC): Promise<string> => {
-    if(imageBaseUrl !== "" || imagePropsName !== undefined)
-    {
+    if (imageBaseUrl !== "" || imagePropsName !== undefined) {
       return new URL(icon[imagePropsName] as string, imageBaseUrl).href;
     }
 
     const candidateProps: IconProps[] = ["uri", "path", "url"];
-    for(const prop of candidateProps)
-    {
-      if(icon[prop])
-      {
+    for (const prop of candidateProps) {
+      if (icon[prop]) {
         const propValue = icon[prop];
-        if(typeof(propValue) !== "string") continue;
+        if (typeof propValue !== "string") continue;
 
-        if(propValue.startsWith("http://") || propValue.startsWith("https://")) return propValue;
-        if(streamer.imagePrefix)
-        {
-          const path = new URL(propValue, streamer.imagePrefix).href;
-          const res = await axios.get(encodeURI(decodeURI(path)), {responseType: "stream"});
-          if(res.status === 200)
-          {
-            imageBaseUrl = streamer.imagePrefix;
+        if (propValue.startsWith("http://") || propValue.startsWith("https://"))
+          return propValue;
+
+        if (streamer.imagePrefix) {
+          try {
+            const path = new URL(propValue, streamer.imagePrefix).href;
+            const res = await axios.get(encodeURI(decodeURI(path)), {
+              responseType: "stream",
+            });
+            if (res.status === 200) {
+              imageBaseUrl = streamer.imagePrefix;
+              imagePropsName = prop;
+              return path;
+            }
+          } catch (error) {
+            logger.debug(`not imagePrefix/.${prop}`);
+          }
+        }
+
+        try {
+          const path = new URL(propValue, originUrl).href;
+          const res = await axios.get(encodeURI(decodeURI(path)), {
+            responseType: "stream",
+          });
+          if (res.status === 200) {
+            imageBaseUrl = originUrl;
             imagePropsName = prop;
             return path;
           }
-        }
-        const path = new URL(propValue, originUrl).href;
-        const res = await axios.get(encodeURI(decodeURI(path)), {responseType: "stream"});
-        if(res.status === 200)
-        {
-          imageBaseUrl = originUrl;
-          imagePropsName = prop;
-          return path;
+        } catch (error) {
+          logger.debug(`not domainOrigin/.${prop}`);
         }
       }
     }
 
-    if(streamer.imagePrefix)
-    {
-      const path = new URL(icon.name, streamer.imagePrefix).href;
-      const res = await axios.get(encodeURI(decodeURI(path)), {responseType: "stream"});
-      if(res.status === 200)
-      {
-        imageBaseUrl = streamer.imagePrefix;
-        imagePropsName = "name";
-        return path;
+    if (streamer.imagePrefix) {
+      try {
+        const path = new URL(icon.name, streamer.imagePrefix).href;
+        const res = await axios.get(encodeURI(decodeURI(path)), {
+          responseType: "stream",
+        });
+        if (res.status === 200) {
+          imageBaseUrl = streamer.imagePrefix;
+          imagePropsName = "name";
+          return path;
+        }
+      } catch (error) {
+        logger.debug("not imagePrefix/.name");
       }
     }
     /**
      * dccon subpath가 기본인 듯?
      */
-    const path1 = new URL(`images/${icon.name}`, originUrl).href;
-    const res1 = await axios.get(encodeURI(decodeURI(path1)), {responseType: "stream"});
-    if(res1.status === 200)
-    {
-      imageBaseUrl = `${originUrl}/images/`;
-      imagePropsName = "name";
-      return path1;
+    try {
+      const path1 = new URL(`images/${icon.name}`, originUrl).href;
+      const res1 = await axios.get(encodeURI(decodeURI(path1)), {
+        responseType: "stream",
+      });
+      if (res1.status === 200) {
+        imageBaseUrl = `${originUrl}/images/`;
+        imagePropsName = "name";
+        return path1;
+      }
+    } catch (error) {
+      logger.debug(`not domainOrigin/images/.name`);
     }
-  
-    const path2 = new URL(`images/dccon/${icon.name}`, originUrl).href;
-    const res2 = await axios.get(encodeURI(decodeURI(path2)), {responseType: "stream"});
-    if(res2.status === 200)
-    {
-      imageBaseUrl = `${originUrl}/images/dccon/`;
-      imagePropsName = "name";
-      return path2;
+
+    try {
+      const path2 = new URL(`images/dccon/${icon.name}`, originUrl).href;
+      const res2 = await axios.get(encodeURI(decodeURI(path2)), {
+        responseType: "stream",
+      });
+      if (res2.status === 200) {
+        imageBaseUrl = `${originUrl}/images/dccon/`;
+        imagePropsName = "name";
+        return path2;
+      }
+    } catch (error) {
+      logger.debug(`not domainOrigin/images/dccon/.name`);
     }
-  
-    throw new Error(`Cannot find working url for ${streamer.name} - ${icon.tags[0]}`);
-  }
-  
+
+    throw new Error(
+      `Cannot find working url for ${streamer.name} - ${icon.tags[0]}`
+    );
+  };
 
   /**
-   * 원본 아이콘 index 각각에 대해서 
-   * 이미지 저장, 축소된 이미지 저장을 하고 
+   * 원본 아이콘 index 각각에 대해서
+   * 이미지 저장, 축소된 이미지 저장을 하고
    * 해당 정보를 가진 새 객체를 리턴하도록 함.
-   * 
+   *
    * 각각이 Promise를 리턴하도록 해서 Promise.all을 통해서
    * newIconsData변수에 새로운 배열을 할당함.
    */
-  const newIconsData = await Promise.all(jsonData.dcConsData.map(async (icon: IconBridgeBBCC): Promise<Icon> => {
-    /**
-     * 일관성을 위해서 원본 아이콘 index에 tags가 비었다면
-     * 미지정이라는 값을 추가함.
-     * 
-     * 어차피 아이콘 치환은 keywords에 있는 값으로만 이루어지므로 상관 없음.
-     */
-    if(icon.tags === undefined || icon.tags.length === 0) icon.tags = ["미지정"];
+  const newIconsData = await Promise.all(
+    jsonData.dcConsData.map(async (icon: IconBridgeBBCC): Promise<Icon> => {
+      /**
+       * 일관성을 위해서 원본 아이콘 index에 tags가 비었다면
+       * 미지정이라는 값을 추가함.
+       *
+       * 어차피 아이콘 치환은 keywords에 있는 값으로만 이루어지므로 상관 없음.
+       */
+      if (icon.tags === undefined || icon.tags.length === 0)
+        icon.tags = ["미지정"];
 
-    /**
-     * nameHash를 md5로 만듬.
-     * 태그[0].키워드[0]
-     * keywords[0]은 존재함이 보장됨.
-     * 
-     * hash로 왜 만드는가?
-     * 주소로 이미지를 요청할 때 uri encoding하지 않아도 되는 장점이 있음.
-     * 식별자로 활용하기 좋음.
-     */
-    const iconHash = createHash("md5").update(`${icon.tags[0]}.${icon.keywords[0]}`).digest('hex');
-    /**
-     * 실제 아이콘 이미지 주소 찾기
-     * 스트리머가 지정하지 않은 경우도 존재하기 때문.
-     */
-    const iconUri = await findIconUri(icon);
-    
-    /**
-     * BridgeBBCC 포맷에서는 name이 파일이름을 가리킴.
-     */
-    const iconExt = extname(icon.name) || ".jpg";
-    const newIcon: Icon = {
-      name: icon.name,
-      nameHash: iconHash,
-      // 여기서 저장되는 basePath는 `/`으로 시작하는 앱이 구동되는 서버의 절대경로임.
-      uri: `${basePath}/${iconHash}${iconExt}`,
-      thumbnailUri: `${basePath}/${iconHash}${iconExt}?small`,
-      keywords: icon.keywords,
-      tags: icon.tags,
-      // 이 값이 true이면 uri로 접근하지 않고 originUri로 접근함.
-      useOrigin: false,
-      originUri: iconUri
-    };
+      /**
+       * nameHash를 md5로 만듬.
+       * 태그[0].키워드[0]
+       * keywords[0]은 존재함이 보장됨.
+       *
+       * hash로 왜 만드는가?
+       * 주소로 이미지를 요청할 때 uri encoding하지 않아도 되는 장점이 있음.
+       * 식별자로 활용하기 좋음.
+       */
+      const iconHash = createHash("md5")
+        .update(`${icon.tags[0]}.${icon.keywords[0]}`)
+        .digest("hex");
+      /**
+       * 실제 아이콘 이미지 주소 찾기
+       * 스트리머가 지정하지 않은 경우도 존재하기 때문.
+       */
+      const iconUri = await findIconUri(icon);
 
-
-    try 
-    {
-      // 원본 이미지 저장
-      await saveImage(newIcon.originUri, newIcon.uri, logger);
-      // 축소된 이미지 저장
-      await saveThumbnail(newIcon.uri, `${basePathThumbnail}/${iconHash}${iconExt}`, logger);
-      // 성공하면 새 아이콘 정보 리턴.
-      return newIcon;
-    }
-    catch(err)
-    {
-      logger.error(err);
-      logger.error(icon);
-      logger.error(`use origin uri`)
-      return {
-        ...newIcon,
-        useOrigin: true
+      /**
+       * BridgeBBCC 포맷에서는 name이 파일이름을 가리킴.
+       */
+      const iconExt = extname(icon.name) || ".jpg";
+      const newIcon: Icon = {
+        name: icon.name,
+        nameHash: iconHash,
+        // 여기서 저장되는 basePath는 `/`으로 시작하는 앱이 구동되는 서버의 절대경로임.
+        uri: `${basePath}/${iconHash}${iconExt}`,
+        thumbnailUri: `${basePath}/${iconHash}${iconExt}?small`,
+        keywords: icon.keywords,
+        tags: icon.tags,
+        // 이 값이 true이면 uri로 접근하지 않고 originUri로 접근함.
+        useOrigin: false,
+        originUri: iconUri,
       };
-    }
-  }));
+
+      try {
+        // 원본 이미지 저장
+        await saveImage(newIcon.originUri, newIcon.uri, logger);
+        // 축소된 이미지 저장
+        await saveThumbnail(
+          newIcon.uri,
+          `${basePathThumbnail}/${iconHash}${iconExt}`,
+          logger
+        );
+        // 성공하면 새 아이콘 정보 리턴.
+        return newIcon;
+      } catch (err) {
+        logger.error(err);
+        logger.error(icon);
+        logger.error(`use origin uri`);
+        return {
+          ...newIcon,
+          useOrigin: true,
+        };
+      }
+    })
+  );
   logger.info(`Download Icons done! -> ${basePath}`);
 
-  const failedListJson: {[key: string]: Icon} = {};
-  for(const icon of newIconsData)
-  {
+  const failedListJson: { [key: string]: Icon } = {};
+  for (const icon of newIconsData) {
     // useOrigin=true인 것은 이미지 다운로드, 처리에 실패한 것들 뿐임.
-    if(icon.useOrigin)
-    {
+    if (icon.useOrigin) {
       /**
        * nameHash를 키로 사용함.
        * 혹시 에러가 발생할까봐 name항목을 설정했음.
@@ -208,7 +249,7 @@ export const processor = async (streamer: StreamerData, jsonData: IconIndexBridg
   const finalData = {
     icons: newIconsData,
     timestamp: Date.now(),
-  }
+  };
   await saveJsonFile(finalData, `${basePath}/${INDEX_FILE}`, logger);
   logger.info(`Save Index done! -> ${basePath}/${INDEX_FILE}`);
-}
+};
--- src/iconProcessor/opendccon.ts
+++ src/iconProcessor/opendccon.ts
@@ -3,8 +3,8 @@ import { createHash } from "crypto";
 import { extname } from "path";
 import { existsSync, mkdirSync } from "fs";
 
-import { 
-  IconIndexOpenDccon, 
+import {
+  IconIndexOpenDccon,
   StreamerData,
   Icon,
   IconProps,
@@ -14,20 +14,29 @@ import { FAILED_LIST_FILE, INDEX_FILE } from "../constants";
 import {
   getImageBasePath,
   getThumbnailBasePath,
-  saveImage, 
-  saveThumbnail, 
+  saveImage,
+  saveThumbnail,
   saveJsonFile,
 } from "../functions";
 
 import Logger from "../logger";
 
-export const indexDownloader = async (url: string): Promise<IconIndexOpenDccon> => {
-  const res = await axios.get(encodeURI(decodeURI(`${url}${url.includes("?") ? "&" : "?"}ts=${Date.now()}`)));
+export const indexDownloader = async (
+  url: string
+): Promise<IconIndexOpenDccon> => {
+  const res = await axios.get(
+    encodeURI(
+      decodeURI(`${url}${url.includes("?") ? "&" : "?"}ts=${Date.now()}`)
+    )
+  );
   const jsonData: IconIndexOpenDccon = res.data;
   return jsonData;
-}
+};
 
-export const processor = async (streamer: StreamerData, jsonData: IconIndexOpenDccon): Promise<void> => {
+export const processor = async (
+  streamer: StreamerData,
+  jsonData: IconIndexOpenDccon
+): Promise<void> => {
   const logger = Logger(`${module.filename} [${streamer.name}]`);
   const basePath = getImageBasePath(streamer.name);
   const basePathThumbnail = getThumbnailBasePath(streamer.name);
@@ -36,123 +45,143 @@ export const processor = async (streamer: StreamerData, jsonData: IconIndexOpenD
   let imageBaseUrl = "";
   let imagePropsName: IconProps;
 
-  if(!existsSync(basePath)) mkdirSync(basePath, {recursive: true});
-  if(!existsSync(basePathThumbnail)) mkdirSync(basePathThumbnail, {recursive: true});
+  if (!existsSync(basePath)) mkdirSync(basePath, { recursive: true });
+  if (!existsSync(basePathThumbnail))
+    mkdirSync(basePathThumbnail, { recursive: true });
 
   const findIconUri = async (icon: IconOpenDccon): Promise<string> => {
-    if(icon.path.startsWith("http://") || icon.path.startsWith("https://"))
-    {
+    if (icon.path.startsWith("http://") || icon.path.startsWith("https://")) {
       imageBaseUrl = "";
       imagePropsName = "path";
       return icon.path;
     }
-    
+
     /**
      * 발견한 주소를 임시 저장해서 비효율적인 요청을 막음.
      */
-    if(imageBaseUrl !== "" || imagePropsName !== undefined)
-    {
+    if (imageBaseUrl !== "" || imagePropsName !== undefined) {
       return new URL(icon[imagePropsName] as string, imageBaseUrl).href;
     }
 
-    if(streamer.imagePrefix)
-    {
-      const path = new URL(icon.path, streamer.imagePrefix).href;
-      const res = await axios.get(encodeURI(decodeURI(path)), {responseType: "stream"});
-      if(res.status === 200)
-      {
-        imageBaseUrl = streamer.imagePrefix;
-        imagePropsName = "path";
-        return path;
+    if (streamer.imagePrefix) {
+      try {
+        const path = new URL(icon.path, streamer.imagePrefix).href;
+        const res = await axios.get(encodeURI(decodeURI(path)), {
+          responseType: "stream",
+        });
+        if (res.status === 200) {
+          imageBaseUrl = streamer.imagePrefix;
+          imagePropsName = "path";
+          return path;
+        }
+      } catch (error) {
+        logger.debug(`not imagePrefix/.path`);
       }
-    }
-    else 
-    {
-      const path = new URL(icon.path, originUrl).href;
-      const res = await axios.get(encodeURI(decodeURI(path)), {responseType: "stream"});
-      if(res.status === 200)
-      {
-        imageBaseUrl = originUrl;
-        imagePropsName = "path";
-        return path;
+    } else {
+      try {
+        const path = new URL(icon.path, originUrl).href;
+        const res = await axios.get(encodeURI(decodeURI(path)), {
+          responseType: "stream",
+        });
+        if (res.status === 200) {
+          imageBaseUrl = originUrl;
+          imagePropsName = "path";
+          return path;
+        }
+      } catch (error) {
+        logger.debug(`not domainOrigin/.path`);
       }
     }
 
     const candidateProps: IconProps[] = ["uri", "url"];
-    for(const prop of candidateProps)
-    {
-      if(icon[prop])
-      {
+    for (const prop of candidateProps) {
+      if (icon[prop]) {
         const propValue = icon[prop];
-        if(typeof(propValue) !== "string") continue;
+        if (typeof propValue !== "string") continue;
 
-        if(propValue.startsWith("http://") || propValue.startsWith("https://")) return propValue;
-        if(streamer.imagePrefix)
-        {
-          const path = new URL(propValue, streamer.imagePrefix).href;
-          const res = await axios.get(encodeURI(decodeURI(path)), {responseType: "stream"});
-          if(res.status === 200)
-          {
-            imageBaseUrl = streamer.imagePrefix;
+        if (propValue.startsWith("http://") || propValue.startsWith("https://"))
+          return propValue;
+        if (streamer.imagePrefix) {
+          try {
+            const path = new URL(propValue, streamer.imagePrefix).href;
+            const res = await axios.get(encodeURI(decodeURI(path)), {
+              responseType: "stream",
+            });
+            if (res.status === 200) {
+              imageBaseUrl = streamer.imagePrefix;
+              imagePropsName = prop;
+              return path;
+            }
+          } catch (error) {
+            logger.debug(`not imagePrefix/.${prop}`);
+          }
+        }
+
+        try {
+          const path = new URL(propValue, originUrl).href;
+          const res = await axios.get(encodeURI(decodeURI(path)), {
+            responseType: "stream",
+          });
+          if (res.status === 200) {
+            imageBaseUrl = originUrl;
             imagePropsName = prop;
             return path;
           }
-        }
-        const path = new URL(propValue, originUrl).href;
-        const res = await axios.get(encodeURI(decodeURI(path)), {responseType: "stream"});
-        if(res.status === 200)
-        {
-          imageBaseUrl = originUrl;
-          imagePropsName = prop;
-          return path;
+        } catch (error) {
+          logger.debug(`not domainOrigin/.${prop}`);
         }
       }
     }
 
-    throw new Error(`Cannot find working url for ${streamer.name} - ${icon.tags[0]}`);
-  }
+    throw new Error(
+      `Cannot find working url for ${streamer.name} - ${icon.tags[0]}`
+    );
+  };
 
-
-  const newIconsData = await Promise.all(jsonData.dccons.map(async (icon: IconOpenDccon): Promise<Icon> => {
-    if(icon.tags === undefined || icon.tags.length === 0) icon.tags = ["미지정"];
-    const iconHash = createHash("md5").update(`${icon.tags[0]}.${icon.keywords[0]}`).digest('hex');
-    const iconExt = extname(icon.path) || ".jpg";
-    const iconUri = await findIconUri(icon);
-    const newIcon: Icon = {
-      name: `${icon.keywords[0]}${iconExt}`,
-      nameHash: iconHash,
-      uri: `${basePath}/${iconHash}${iconExt}`,
-      thumbnailUri: `${basePath}/${iconHash}${iconExt}?small`,
-      keywords: icon.keywords,
-      tags: icon.tags,
-      useOrigin: false,
-      originUri: iconUri
-    };
-    
-    try 
-    {
-      await saveImage(newIcon.originUri, newIcon.uri, logger);
-      await saveThumbnail(newIcon.uri, `${basePathThumbnail}/${iconHash}${iconExt}`, logger);
-      return newIcon;
-    }
-    catch(err)
-    {
-      logger.error(err);
-      logger.error(icon);
-      logger.error(`use origin uri`)
-      return {
-        ...newIcon,
-        useOrigin: true
+  const newIconsData = await Promise.all(
+    jsonData.dccons.map(async (icon: IconOpenDccon): Promise<Icon> => {
+      if (icon.tags === undefined || icon.tags.length === 0)
+        icon.tags = ["미지정"];
+      const iconHash = createHash("md5")
+        .update(`${icon.tags[0]}.${icon.keywords[0]}`)
+        .digest("hex");
+      const iconExt = extname(icon.path) || ".jpg";
+      const iconUri = await findIconUri(icon);
+      const newIcon: Icon = {
+        name: `${icon.keywords[0]}${iconExt}`,
+        nameHash: iconHash,
+        uri: `${basePath}/${iconHash}${iconExt}`,
+        thumbnailUri: `${basePath}/${iconHash}${iconExt}?small`,
+        keywords: icon.keywords,
+        tags: icon.tags,
+        useOrigin: false,
+        originUri: iconUri,
       };
-    }
-  }));
+
+      try {
+        await saveImage(newIcon.originUri, newIcon.uri, logger);
+        await saveThumbnail(
+          newIcon.uri,
+          `${basePathThumbnail}/${iconHash}${iconExt}`,
+          logger
+        );
+        return newIcon;
+      } catch (err) {
+        logger.error(err);
+        logger.error(icon);
+        logger.error(`use origin uri`);
+        return {
+          ...newIcon,
+          useOrigin: true,
+        };
+      }
+    })
+  );
   logger.info(`Download Icons done! -> ${basePath}`);
 
-  const failedListJson: {[key: string]: Icon} = {};
-  for(const icon of newIconsData)
-  {
-    if(icon.useOrigin)
-    {
+  const failedListJson: { [key: string]: Icon } = {};
+  for (const icon of newIconsData) {
+    if (icon.useOrigin) {
       failedListJson[icon.nameHash || icon.name] = icon;
     }
   }
@@ -162,7 +191,7 @@ export const processor = async (streamer: StreamerData, jsonData: IconIndexOpenD
   const finalData = {
     icons: newIconsData,
     timestamp: Date.now(),
-  }
+  };
   await saveJsonFile(finalData, `${basePath}/${INDEX_FILE}`, logger);
   logger.info(`Save Index done! -> ${basePath}/${INDEX_FILE}`);
-}
+};
