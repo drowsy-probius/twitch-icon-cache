5bb9ab56940b8308af957a56718c23e52fdfc2c0
drowsy-probius <k123s456h@gmail.com>
2023-05-12 15:54:43 +0900

    fix: error handling

--- src/background.ts
+++ src/background.ts
@@ -38,41 +38,54 @@ class Cronjob
     this.timerIdentifier = setInterval(this.job, this.cacheTime);
   }
 
-  job()
+  async job()
   {
-    logger.info(`execute cronjob on ${(new Date()).toString()}`);
-    STREAMER_DATA.forEach(async streamer => {
-      // 로컬에 저장되어있을 이미지 정보 json 파일 경로
-      const jsonPath = resolve(join(getImageBasePath(streamer.name), INDEX_FILE));
-      // 로컬에 저장되어 있는 json파일이 있으면 업데이트할 지 결정함.
-      if(fs.existsSync(jsonPath))
-      {
-        // 이미지 정보 json 파일 읽음.
-        const data = fs.readFileSync(jsonPath, "utf8");
-        // json으로 파싱
-        const json: IconIndex = JSON.parse(data);
-        // 로컬 json에 다운받았을 때 시간 정보
-        const timestamp = json.timestamp;
-        /**
-         * 캐시 시간보다 작으면 정보를 새로 다운로드 하지 않음.
-         * 로컬 시간 값을 먼저 비교하는 이유는 최대한 원본 서버에 부하를 줄이기 위함.
-         */
-        if((Date.now() - timestamp) <= this.cacheTime)
-        {
-          logger.info(`[Cronjob] ${streamer.name}'s data is up-to-date by timestamp. Skip downloading...`);
-          return;
+    logger.info(`execute cronjob on ${new Date().toString()}`);
+
+    for (const streamer of STREAMER_DATA) {
+      try {
+        // 로컬에 저장되어있을 이미지 정보 json 파일 경로
+        const jsonPath = resolve(
+          join(getImageBasePath(streamer.name), INDEX_FILE)
+        );
+        // 로컬에 저장되어 있는 json파일이 있으면 업데이트할 지 결정함.
+        if (fs.existsSync(jsonPath)) {
+          // 이미지 정보 json 파일 읽음.
+          const data = fs.readFileSync(jsonPath, "utf8");
+          // json으로 파싱
+          const json: IconIndex = JSON.parse(data);
+          // 로컬 json에 다운받았을 때 시간 정보
+          const timestamp = json.timestamp;
+          /**
+           * 캐시 시간보다 작으면 정보를 새로 다운로드 하지 않음.
+           * 로컬 시간 값을 먼저 비교하는 이유는 최대한 원본 서버에 부하를 줄이기 위함.
+           */
+          if (Date.now() - timestamp <= this.cacheTime) {
+            logger.info(
+              `[Cronjob] ${streamer.name}'s data is up-to-date by timestamp. Skip downloading...`
+            );
+            return;
+          }
+
+          // 로컬의 icons값과 원격에서 가져온 icons값을 정해진 함수로 비교함.
+          if (
+            !doUpdateJson(
+              json.icons,
+              await indexDownloader[streamer.type](streamer.url)
+            )
+          ) {
+            logger.info(
+              `[Cronjob] ${streamer.name}'s data is up-to-date by compare function. Skip downloading...`
+            );
+            return;
+          }
         }
 
-        // 로컬의 icons값과 원격에서 가져온 icons값을 정해진 함수로 비교함.
-        if(!doUpdateJson(json.icons, (await indexDownloader[streamer.type](streamer.url))))
-        {
-          logger.info(`[Cronjob] ${streamer.name}'s data is up-to-date by compare function. Skip downloading...`);
-          return;
-        }
+        // 원격 서버에 새로운 데이터가 있다고 판단되어 새로 다운함.
+        processorFunctions(streamer);
+      } catch (e) {
+        logger.error(e);
       }
-      
-      // 원격 서버에 새로운 데이터가 있다고 판단되어 새로 다운함.
-      processorFunctions(streamer);
-    });
+    }
   }
 }
